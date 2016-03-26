ecc5541599ffadfdcf32a5b8e2b7986d3a905bb5
dassiorleando <dassi_orleando@yahoo.fr>
2016-03-26 15:34:29 +0100

    Display consultations

--- app/scripts/services/consultationservice.js
+++ app/scripts/services/consultationservice.js
@@ -8,6 +8,21 @@
  * Service in the mytodoApp.
  */
 angular.module('demoApp')
-  .service('ConsultationService', function () {
+  .service('ConsultationService', function ($http) {
     // AngularJS will instantiate a singleton by calling "new" on this function
+
+    var service = [];
+    var url = "http://"+document.location.hostname+":8080";
+
+    //load patients
+    service.getAll = function(page) {
+      return $http.get(url+"/api/consultations/"+ page)
+    };
+
+    // Save a new patient
+    service.save = function(patient) {
+      return $http.post(url+"/api/consultations", patient);
+    };
+
+    return service;
   });
